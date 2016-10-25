c965150d4a9d91fdccf2f581aaae37bfaf52367f
Rick Ho <rivenhe@foxmail.com>
2016-10-25 22:24:50 +0800

    addUglify

--- h5graphics/Gruntfile.js
+++ h5graphics/Gruntfile.js
@@ -1,2 +1,16 @@
+vueFiles = ['bower_components/vue/dist/vue.min.js'];
+
 module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			vue: {
+				files: {
+					'dists/vue.js': vueFiles
+				}
+			}
+		}
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', [ 'uglify' ]);
 };
